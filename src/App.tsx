143ad1c83529dda2d7d9631d85cb035a6b59a745
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-02-27 13:56:18 +0900

    Fix ESLint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React from "react";
 
-function App() {
+function App(): JSX.Element {
   return (
     <>
       <div>Hello World!</div>
