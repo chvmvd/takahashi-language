f42096ae16ba4449ff11277fbc0692c942887bcc
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-02-27 16:48:36 +0900

    Create takahashi language playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,191 +1,72 @@
-import React, {
-  useState,
-  useEffect,
-  type Dispatch,
-  type SetStateAction,
-} from "react";
-import { type TakahashiLangSyntaxStatus } from "./components/takahashiLangDefinitions";
+import React, { useState, useEffect } from "react";
+import {
+  availableTakahashiLangCommands,
+  type TakahashiLangSyntaxStatus,
+} from "./components/takahashiLangDefinitions";
+import takahashiLangInterpreter from "./components/takahashiLangInterpreter";
 import takahashiLangSyntaxChecker from "./components/takahashiLangSyntaxChecker";
 import {
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  Dialog,
-  DialogTitle,
-  Paper,
-  TableContainer,
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
-  TextField,
-} from "@mui/material";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
-
-function AppBarComponent(): JSX.Element {
-  const [open, setOpen] = useState<boolean>(false);
-  return (
-    <>
-      <AppBar>
-        <Toolbar>
-          <Typography variant="h5" component="h1" flexGrow={1}>
-            Takahashi Language Playground
-          </Typography>
-          <IconButton
-            color="inherit"
-            size="large"
-            href="https://chvmvd.github.io/takahashi-language/"
-          >
-            <GitHubIcon fontSize="inherit" />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            size="large"
-            onClick={() => {
-              setOpen(true);
-            }}
-          >
-            <HelpOutlineOutlinedIcon fontSize="inherit" />
-          </IconButton>
-          <Dialog
-            open={open}
-            onClose={() => {
-              setOpen(false);
-            }}
-          >
-            <DialogTitle>Available Commands</DialogTitle>
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Command</TableCell>
-                    <TableCell>Meaning</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>
-                      <code>高橋</code>
-                    </TableCell>
-                    <TableCell>Increment the data pointer.</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <code>髙橋</code>
-                    </TableCell>
-                    <TableCell>Decrement the data pointer.</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <code>高𣘺</code>
-                    </TableCell>
-                    <TableCell>
-                      Increment the byte at the data pointer.
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <code>髙𣘺</code>
-                    </TableCell>
-                    <TableCell>
-                      Decrement the byte at the data pointer.
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <code>高𫞎</code>
-                    </TableCell>
-                    <TableCell>Output the byte at the data pointer.</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <code>髙𫞎</code>
-                    </TableCell>
-                    <TableCell>
-                      Receive one byte from input and store it in the byte at
-                      the data pointer.
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <code>高槗</code>
-                    </TableCell>
-                    <TableCell>
-                      If the byte at the data pointer is zero, jump the
-                      instruction pointer forward to the command after the
-                      matching <code>髙槗</code> command.
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <code>髙槗</code>
-                    </TableCell>
-                    <TableCell>
-                      If the byte at the data pointer is nonzero, jump the
-                      instruction pointer back to the command after the matching{" "}
-                      <code>高槗</code> command.
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Dialog>
-        </Toolbar>
-      </AppBar>
-    </>
-  );
-}
-
-function ProgramComponent({
-  syntaxStatus,
-  sourceCode,
-  setSourceCode,
-}: {
-  syntaxStatus: TakahashiLangSyntaxStatus;
-  sourceCode: string;
-  setSourceCode: Dispatch<SetStateAction<string>>;
-}): JSX.Element {
-  return (
-    <>
-      <Typography variant="h5" component="h2">
-        Program
-      </Typography>
-      <TextField
-        variant="outlined"
-        error={syntaxStatus !== "OK"}
-        helperText={syntaxStatus === "OK" ? "" : syntaxStatus}
-        placeholder="Write your code here."
-        multiline
-        rows={10}
-        fullWidth
-        value={sourceCode}
-        onChange={(e) => {
-          setSourceCode(e.target.value);
-        }}
-      />
-    </>
-  );
-}
+  type Memory,
+  defaultMemory,
+} from "./components/brainfuck-playground/src/components/brainfuckDefinitions";
+import { Toolbar, Box, Stack } from "@mui/material";
+import {
+  AppBarComponent,
+  ProgramComponent,
+  InputComponent,
+  RunButtonComponent,
+  OutputComponent,
+  MemoryComponent,
+} from "./components/brainfuck-playground/src/components/UIComponents";
 
 function App(): JSX.Element {
   const [sourceCode, setSourceCode] = useState<string>("");
   const [syntaxStatus, setSyntaxStatus] =
     useState<TakahashiLangSyntaxStatus>("OK");
+  const [input, setInput] = useState<string>("");
+  const [output, setOutput] = useState<string>("");
+  const [memory, setMemory] = useState<Memory>(defaultMemory);
+  const [pointer, setPointer] = useState<number>(0);
   useEffect(() => {
     setSyntaxStatus(takahashiLangSyntaxChecker(sourceCode));
   }, [sourceCode]);
   return (
     <>
-      <AppBarComponent />
-      <Toolbar />
-      <ProgramComponent
-        syntaxStatus={syntaxStatus}
-        sourceCode={sourceCode}
-        setSourceCode={setSourceCode}
+      <AppBarComponent
+        GitHubUrl="https://github.com/chvmvd/takahashi-language/"
+        availableCommands={availableTakahashiLangCommands}
       />
+      <Toolbar />
+      <Box sx={{ m: 2 }}>
+        <Stack spacing={2}>
+          <Stack spacing={1}>
+            <ProgramComponent<TakahashiLangSyntaxStatus>
+              syntaxStatus={syntaxStatus}
+              sourceCode={sourceCode}
+              setSourceCode={setSourceCode}
+            />
+            <Stack direction="row" spacing={2} alignItems="flex-end">
+              <Box flexGrow={1}>
+                <InputComponent input={input} setInput={setInput} />
+              </Box>
+              <RunButtonComponent
+                onClick={() => {
+                  if (syntaxStatus === "OK") {
+                    const result = takahashiLangInterpreter(sourceCode, input);
+                    setOutput(result.output);
+                    setMemory(result.memory);
+                    setPointer(result.pointer);
+                  }
+                }}
+              />
+            </Stack>
+          </Stack>
+          <Stack spacing={1}>
+            <OutputComponent output={output} />
+            <MemoryComponent memory={memory} pointer={pointer} />
+          </Stack>
+        </Stack>
+      </Box>
     </>
   );
 }
