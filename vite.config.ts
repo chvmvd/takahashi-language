905ba0233b55a9749fb77445cc7cc70dc1d5d111
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-10 17:20:11 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.BASE_URL,
+  base: process.env.GITHUB_ACTIONS ? process.env.BASE_URL : "/",
 });
